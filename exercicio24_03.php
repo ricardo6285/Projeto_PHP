7f03049dcf6c5e0f9ccfede46406ad108998c34d
ricardo6285 <samucabrindes@gmail.com>
2026-04-10 12:04:29 -0300

    Exercicio de Alunos com função

--- exercicio24_03.php
+++ exercicio24_03.php
@@ -1,38 +1,38 @@
 <?php 
-// $nota1=0;
-// $nota2=0;
-// $aluno;
-// $aprovados=0;
-// $reprovados=0;
-// $exame=0;
-// $mediaclasse=0;
+$nota1=0;
+$nota2=0;
+$aluno;
+$aprovados=0;
+$reprovados=0;
+$exame=0;
+$mediaclasse=0;
 
-// for($aluno = 0; $aluno <=5;$aluno++){
-// 	echo "Aluno $aluno <br/>";
-// $nota1 =10; $nota2 =10;
-// echo"Nota1::$nota1 e Nota2:: $nota2<br>";
-// $media = ($nota1 + $nota2) / 2;
-// echo "Media $media<br>";
-// $mediaclasse = $mediaclasse + $media;
+for($aluno = 0; $aluno <=5;$aluno++){
+	echo "Aluno $aluno <br/>";
+$nota1 =10; $nota2 =10;
+echo"Nota1::$nota1 e Nota2:: $nota2<br>";
+$media = ($nota1 + $nota2) / 2;
+echo "Media $media<br>";
+$mediaclasse = $mediaclasse + $media;
 
-// if($media < 3 ){
-// echo "reprovado<br>";$reprovados++;	
-// }elseif ($media >= 3.0 && $media <= 7.0) {
-// 	echo "Exame<br>";$exame++;
-// }else {
-// 	echo "Aprovado<br/>";$aprovados++;
-// }
+if($media < 3 ){
+echo "reprovado<br>";$reprovados++;	
+}elseif ($media >= 3.0 && $media <= 7.0) {
+	echo "Exame<br>";$exame++;
+}else {
+	echo "Aprovado<br/>";$aprovados++;
+}
 
-// }
-// $mediaclasse = $mediaclasse / 6;
-// echo "$exame foram para exame<br>";
-// echo "$reprovados foram reprovados<br>";
-// echo "$aprovados foram aprovados<br>";
-// echo "Media da classe $mediaclasse";
+}
+$mediaclasse = $mediaclasse / 6;
+echo "$exame foram para exame<br>";
+echo "$reprovados foram reprovados<br>";
+echo "$aprovados foram aprovados<br>";
+echo "Media da classe $mediaclasse";
 
 //======== Exercicio 2 ======================================
 
-$total = 240;
+/*$total = 240;
 
 while ($total > 0) {
 
@@ -81,7 +81,7 @@ if ($total == 0) {
     echo "Casa lotada!\n";
 }
 
-echo "Sistema encerrado\n";
+echo "Sistema encerrado\n";*/
 
 
 
