1f4d6edc9e52280277dbb5f9ed223d85fa4cfefd
ricardo6285 <samucabrindes@gmail.com>
2026-04-14 20:19:41 -0300

    vetores

---
+++ array_linha.php
@@ -0,0 +1,34 @@
+<?php 
+
+// linha = vetor
+// linha-coluna = matriz 
+$idade = array();
+
+$idade[0] = 10;
+$idade[8] = 5;
+
+$valor = array(10,20,30,40);
+
+$nomes= array ("Maria","José","Pedro","Paulo");
+
+
+$salario[] = 1500;
+$salario[] = 1250.35;
+$salario[] = 1587.96;
+$salario[] = 1587.89;
+
+echo ($valor[0] . "<br>");
+echo ($valor[3] . "<br>");
+echo($salario[2] . "<br>");
+
+
+
+var_dump($valor);
+echo("<br>");
+var_dump($nomes);
+echo("<br>");
+var_dump($idade);
+echo("<br>");
+var_dump($salario);
+
+ ?>
---
+++ soma_vetores.php
@@ -0,0 +1,21 @@
+<?php 
+
+$num1 = array(10,20,30,40);
+$num1 = array(50,60,70,80);
+
+var_dump($num1);
+echo "<br>";
+var_dump($num1);
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
