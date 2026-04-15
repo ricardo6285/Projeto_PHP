b396e186cb8d13f6a12b5be1c8cf9c5fef2f38a0
ricardo6285 <samucabrindes@gmail.com>
2026-04-14 22:16:28 -0300

    exercicio de vetores

---
+++ exercicio_criacao_de_vetores.php
@@ -0,0 +1,24 @@
+<?php 
+
+$numeros = array(12,-5,0,7,-3,8,10,-1,4,6);
+$resultado = array();
+
+
+for ($i=0; $i <count($numeros) ; $i++) { 
+	if($resultado[$i] > 0 ){
+	echo "$resultado";	
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
