f403b9d7a92fc5e3d4d66d7297e6d8c061510628
ricardo6285 <samucabrindes@gmail.com>
2026-04-15 12:40:17 -0300

    exercicios 14-04

--- exercicio_criacao_de_vetores.php
+++ exercicio_criacao_de_vetores.php
@@ -1,18 +1,41 @@
 <?php 
 
 $numeros = array(12,-5,0,7,-3,8,10,-1,4,6);
-$resultado = array();
+$positivos = 0;
+$negativos = 0;
+$pares = 0;
+$impares = 0;
 
-
+// percorre a array
 for ($i=0; $i <count($numeros) ; $i++) { 
-	if($resultado[$i] > 0 ){
-	echo "$resultado";	
+
+
+// verifica se é positivo(maior que 0)	
+	if($numeros[$i] > 0 ){
+	$positivos++;	
 	}
+// verifica se é negativo
+	if ($numeros[$i] < 0) {
+		$negativos++;
+	}
+
+
+ // Verificar se é par
+    // Um número é par quando o resto da divisão por 2 é igual a 0
+    if ($numeros[$i] % 2 == 0) {
+    	$pares++;
+    }else{
+    	$impares++;
+    }
+
 }
 
-
-
-
+// Exibir resultados
+echo "Resultados da Análise:<br>";
+echo "Números positivos: $positivos<br>";
+echo "Números negativos: $negativos<br>";
+echo "Números pares: $pares<br>";
+echo "Números impares: $impares<br>";
 
 
 
