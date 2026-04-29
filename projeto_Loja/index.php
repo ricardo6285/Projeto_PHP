e14c5b1faf2c831a54645dde7d4979979335fa05
Ricardo Rodrigues da Silva <samucabrindes@gmail.com>
2026-04-28 21:20:01 -0300

    Add files via upload

---
+++ projeto_Loja/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+</head>
+<body>
+
+<script src="js/script.js"></script>
+</body>
+</html>
